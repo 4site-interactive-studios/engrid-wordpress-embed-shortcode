dd9f00c84278c27d1b2b72948b9d0836e0df4bc8
MichaelW@4Site <michaelw@4sitestudios.com>
2020-08-11 23:32:11 -0400

    implemented unique IDs for iFrames

--- en-iframe.php
+++ en-iframe.php
@@ -18,21 +18,28 @@ function en_iframe_render($atts = [], $content = null, $tag = '')
 {
     // normalize attribute keys, lowercase
     $atts = array_change_key_case((array) $atts, CASE_LOWER);
+
+    $uniqid = uniqid();
+
     // Get URL
     $url = $atts['url'];
     $query_strings = $_SERVER['QUERY_STRING'];
     if($query_strings){
-        // Check if URL has "?"
-        if(strpos($url, "?") === false){
-            $url.='?'.$query_strings;
-        }else{
-            $url.='&'.$query_strings;
-        }
-
+        $query_strings.='&';
     }
-    $output = "<iframe loading='lazy' id='en-iframe' width='100%' scrolling='no' class='en-iframe " . $atts['class'] . "'";
-    $output .= " src='" . $url . "' frameborder='0' allowfullscreen></iframe>";
-    return $output;
+    $query_strings.='en_id='.$uniqid;
+
+    // Check if URL has "?"
+    if(strpos($url, "?") === false){
+        $url.='?'.$query_strings;
+    }else{
+        $url.='&'.$query_strings;
+    }
+
+    $iframe_id = "en-iframe-$uniqid";
+    $iframe_classes = 'en-iframe ' . $atts['class'];
+
+    return "<iframe loading='lazy' id='en-iframe-$uniqid' width='100%' scrolling='no' class='$iframe_classes' src='$url' frameborder='0' allowfullscreen></iframe>";
 }
 add_shortcode('en', 'en_iframe_render');
 
--- js/iframe.js
+++ js/iframe.js
@@ -1,12 +1,15 @@
 window.onmessage = (e) => {
+  var iframe_id = "en-iframe";
+  if(e.data.hasOwnProperty('enID')) {
+    iframe_id += "-" + e.data.enID;
+  }
+
   if (e.data.hasOwnProperty("frameHeight")) {
-    document.getElementById(
-      "en-iframe"
-    ).style.height = `${e.data.frameHeight}px`;
+    document.getElementById(iframe_id).style.height = `${e.data.frameHeight}px`;
   } else if (e.data.hasOwnProperty("scroll") && e.data.scroll) {
     // Only scrolls if that message is not coming from a donation page
     // e.data.scroll will be 1 if it's coming from a donation page
-    document.getElementById("en-iframe").scrollIntoView({
+    document.getElementById(iframe_id).scrollIntoView({
       behavior: "smooth",
     });
     console.log("Scrolling");
