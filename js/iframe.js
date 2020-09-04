3288b55951add1b5466e69c5e8f57b7655304a3d
Michael Wilson <michaelw@4sitestudios.com>
2020-09-04 18:46:17 -0400

    updated method for tracking iframe messages

--- js/iframe.js
+++ js/iframe.js
@@ -1,17 +1,19 @@
-window.onmessage = (e) => {
-  var iframe_id = "en-iframe";
-  if(e.data.hasOwnProperty('enID')) {
-    iframe_id += "-" + e.data.enID;
+function getFrameByEvent(event) {
+  return [].slice.call(document.getElementsByTagName('iframe')).filter(function(iframe) {
+    return iframe.contentWindow === event.source;
+  })[0];
+}
+window.addEventListener("message", function(e) {
+  var frame = getFrameByEvent(e);
+  if(frame) {
+    if(e.data.hasOwnProperty("frameHeight")) {
+      frame.style.height = `${e.data.frameHeight}px`;
+    } else if(e.data.hasOwnProperty("scroll") && e.data.scroll) {
+      // Only scrolls if that message is not coming from a donation page
+      // e.data.scroll will be 1 if it's coming from a donation page
+      frame.scrollIntoView({
+        behavior: "smooth",
+      });  
+    }
   }
-
-  if (e.data.hasOwnProperty("frameHeight")) {
-    document.getElementById(iframe_id).style.height = `${e.data.frameHeight}px`;
-  } else if (e.data.hasOwnProperty("scroll") && e.data.scroll) {
-    // Only scrolls if that message is not coming from a donation page
-    // e.data.scroll will be 1 if it's coming from a donation page
-    document.getElementById(iframe_id).scrollIntoView({
-      behavior: "smooth",
-    });
-    console.log("Scrolling");
-  }
-};
+}, false);
